ca394a73c03a16c49f7aaa7a8dc48a05e27ce23e
Andrewzhang217 <zhangguangzesheng@gmail.com>
2024-04-05 13:48:09 +0800

    Change n, k to be random and threshold

--- relax_backend/functionalDependency/views.py
+++ relax_backend/functionalDependency/views.py
@@ -2,20 +2,23 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 
 from functionalDependency.gen import generate_random_fds, identifyCandidateKeys
+import random
 
 
 class FunctionalDependencyViewSet(viewsets.ViewSet):
     def list(self, request):
         level = request.query_params['difficulty']
         while True:
-            fds, attributes = generate_random_fds(7, 3)
+            n = random.randint(3, 8)
+            k = random.randint(2, 15)
+            fds, attributes = generate_random_fds(n, k)
             candidate_keys, total_cost = identifyCandidateKeys(attributes, fds)
 
-            if level == "easy" and total_cost < 50:
+            if level == "easy" and total_cost < 40:
                 return Response({"attributes": attributes, "fds": fds, "candidate_keys": candidate_keys})
 
-            if level == "medium" and 50 <= total_cost < 100:
+            if level == "medium" and 40 <= total_cost < 80:
                 return Response({"attributes": attributes, "fds": fds, "candidate_keys": candidate_keys})
 
-            if level == "hard" and total_cost > 100:
+            if level == "hard" and total_cost > 80:
                 return Response({"attributes": attributes, "fds": fds, "candidate_keys": candidate_keys})
